16049fc60192777e2f54e7f8541d2cfa676e1704
Milton <miltongutierrez881@gmail.com>
2022-10-01 16:48:45 -0700

    ejemplo 16 con comentarios

--- src/unidad3/ejemplo16.java
+++ src/unidad3/ejemplo16.java
@@ -1,13 +1,17 @@
 package unidad3;
-import javax.swing.JOptionPane;
+
+import javax.swing.JOptionPane; // importa una caja de herramientas de la librerias de java,  javax = nombre de la libreria, swing = clase (caja de herramienta), JOptionPane = herramienta
+
 public class ejemplo16 {
 
 	public static void main(String[] args) {
 		
-		String nombre = "pepito";
-		int edad = 12;
-		JOptionPane.showInternalMessageDialog(null, "Nombre "+nombre+ 
-													"\n Edad "+ edad);
+		String nombre = "pepito"; // Variable de tipo String
+		
+		int edad = 12; // Variable de tipo entero (Int)
+		
+		JOptionPane.showInternalMessageDialog(null, "Nombre "+ nombre + "\n Edad "+ edad); // Herramienta JOptionPane.showInternalMessageDialog con mensaje 
+											  // >>>>>> \n <<<< representa un salto de line o un enter 
 				
 
 	}
