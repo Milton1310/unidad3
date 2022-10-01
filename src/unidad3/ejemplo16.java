3956c6526f2e1ae137ab6630456af2a50be5c64e
Milton <miltongutierrez881@gmail.com>
2022-10-01 09:08:00 -0700

    ejemplo16

---
+++ src/unidad3/ejemplo16.java
@@ -0,0 +1,15 @@
+package unidad3;
+import javax.swing.JOptionPane;
+public class ejemplo16 {
+
+	public static void main(String[] args) {
+		
+		String nombre = "pepito";
+		int edad = 12;
+		JOptionPane.showInternalMessageDialog(null, "Nombre "+nombre+ 
+													"\n Edad "+ edad);
+				
+
+	}
+
+}
