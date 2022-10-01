76596079778486c4644dc0a0218e6d37af13c4f4
Milton <miltongutierrez881@gmail.com>
2022-10-01 16:48:29 -0700

    ejemplo 15 con comentarios

--- src/unidad3/Ejemplo15.java
+++ src/unidad3/Ejemplo15.java
@@ -1,12 +1,16 @@
 
 package unidad3;
-import javax.swing.JOptionPane;
+
+import javax.swing.JOptionPane; // importa una caja de herramientas de la librerias de java,  javax = nombre de la libreria, swing = clase (caja de herramienta), JOptionPane = herramienta
+
 public class Ejemplo15 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-			JOptionPane.showMessageDialog(null, "Mensaje Simple tipo Informacion");
-
+		
+			JOptionPane.showMessageDialog(null, "Mensaje Simple tipo Informacion"); //Utilizamos la herramienta JOptionPane.showMessgeDialog
+										 //Dentro de los parentesis van los parametros
+										 //el primer parámetro, representa el componente padre sobre el cual el mensaje se mostrará, si nosotros no le enviamos ninguno abrira una ventana nueva
+			
 	}
 
 }
