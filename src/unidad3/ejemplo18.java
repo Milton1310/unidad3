39a3e11ec28744017db3d4e02df97660296cd5a3
Milton <miltongutierrez881@gmail.com>
2022-10-01 16:49:05 -0700

    ejemplo 18 con comentarios

--- src/unidad3/ejemplo18.java
+++ src/unidad3/ejemplo18.java
@@ -4,19 +4,19 @@ public class ejemplo18 {
 
 	public static void main(String[] args) {
 		
-		int valor1, valor2, resultado;
-		Scanner entrada = new Scanner(System.in); 
+		int valor1, valor2, resultado;// declaracion de datos numericos enteros
+		Scanner entrada = new Scanner(System.in); //llamar a la herramienta Scanner 
 		
-		System.out.println("Suma :");
+		System.out.println("Suma :");//imprimir "Suma :" en la consola
 		
-		System.out.println("Introdusca el primer valor");
-		valor1 = entrada.nextInt() ;
+		System.out.println("Introdusca el primer valor");//imprimir "Introdusca el primer valor" en consola
+		valor1 = entrada.nextInt();// asignar valor a la variable valor1 y leer lo que se introduce en la consola
 		
-		System.out.println("Introduce el segundo valor");
-		valor2 = entrada.nextInt() ;
+		System.out.println("Introduce el segundo valor");//imprimir "Introduce el segundo valor"
+		valor2 = entrada.nextInt() ;// asignar valor a la variable valor2 y leer lo que se introduce en la consola
 		
-		resultado = valor1 + valor2;
-		System.out.println("El resultado es: "+ resultado);
+		resultado = valor1 + valor2; //asignar a la variable resultado el valor de las variables valor1 y valor2 sumadas
+		System.out.println("El resultado es: "+ resultado);//imprimir resultado
 						
 
 	}
