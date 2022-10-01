a8182cd4cdcbb95e65a1332ae4e16b998daae31a
Milton <miltongutierrez881@gmail.com>
2022-10-01 16:47:27 -0700

    ejemplo 12 con comentarios

--- src/unidad3/ejemplo12.java
+++ src/unidad3/ejemplo12.java
@@ -3,8 +3,8 @@ package unidad3;
 public class ejemplo12 {
 
 	public static void main(String[] args) {
-		System.out.println("hola mundo");
-//hola
+		System.out.println("hola mundo"); // System.out.println("hola mundo") Seria lo equivalente a un Escribir del PSint, esto imprime en consola 
+
 	}
 
 }
