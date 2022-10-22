31edd484cdcfd72a03a3a8476ec7cd274a766fdd
milton <miltongutierrez881@gmail.com>
2022-10-22 12:28:30 -0700

    Ejemplo22 terminado

---
+++ src/unidad3_2/ejemplo26.java
@@ -0,0 +1,23 @@
+package unidad3_2;
+
+public class ejemplo26 {
+
+    public static void main (String[]args){
+
+        int numero = 15;
+
+        if (numero == 15) {
+            System.out.println("Numero es 15");
+        }else {
+            if (numero == 30){
+                System.out.println("Numero es 30");
+            }else {
+                if (numero == 45){
+                    System.out.println("numero es 45");
+                }else{
+                    System.out.println("numero no valido");
+                }
+            }
+        }
+    }
+}
