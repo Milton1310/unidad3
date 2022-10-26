e18d70c096cdf0f2c2bb2c1a1a7e078ae145b95e
milton <miltongutierrez881@gmail.com>
2022-10-25 17:18:26 -0700

    Ejemplo30 terminado

---
+++ src/unidad3_2/ejemplo30.java
@@ -0,0 +1,27 @@
+package unidad3_2;
+
+public class ejemplo30 {
+
+    public static void main(String[]args){
+
+
+        int saludo = 4;
+
+        switch (saludo){
+
+            case 1:
+                System.out.println("BUENOS DIAS");
+                break;
+            case 2:
+                System.out.println("BUENAS TARDES");
+                break;
+            case 3:
+                System.out.println("BUENOS NOCHES");
+                break;
+            default:
+                System.out.println("ERROR EN LA OPCION");
+                break;
+
+        }
+    }
+}
